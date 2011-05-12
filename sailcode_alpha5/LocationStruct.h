abda22ca188d19e4663cd2b9ad9a040dd353ab5d
Laszlo <Laszlo@localhost>
2011-05-12 10:46:42 -0400

    moved struct and menu onto different tabs for readablity

---
+++ sailcode_alpha5/LocationStruct.h
@@ -0,0 +1,19 @@
+//Place to put all structs inorder to allow for passing and returning of structs from functions.
+
+struct points{
+  double latDeg;
+  double latMin;
+  double lonDeg;
+  double lonMin;
+};            
+ //struct for holding course waypoints 
+ 
+points waypoints[10] ={        //optional preset values for registers
+  {44.0,13.6927,-76.0,29.5175}, 
+  {0,0,0,0}, 
+  {0,0,0,0}};
+  //initialization of struct, put default coordinates here
+  
+points stationPoints[4];      //possible additional structures for stationkeeping and 
+                              //course plotting
+points coursePoints[10];  
