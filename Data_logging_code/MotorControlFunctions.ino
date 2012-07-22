db2a746bfd9679ae98d23c8d70141e4e347c5df0
Valerie <Valerie@localhost>
2012-07-22 18:02:35 -0400

    playing with cooking hacks 3G /GPS module

---
+++ Data_logging_code/MotorControlFunctions.ino
@@ -0,0 +1,77 @@
+
+//Pololu servo board test in Mini SSC II mode
+//(mode jumper inplace on Pololu board)
+
+
+
+
+//Servo_command receieves a servo number (acceptable range: 00-FE) and a position (acceptable range: 00-FE)
+//void servo_command(int whichservo, int position, byte longRange) {
+//    servo_ser.print(0xFF, BYTE); //servo control board sync
+//Plolou documentation is wrong on servo numbers in MiniSSCII
+//    servo_ser.print(whichservo+(longRange*8), BYTE); //servo number, 180 mode
+//    servo_ser.print(position, BYTE); //servo position
+//}
+
+//Accept a angle range to turn the rudder to
+//float ang acceptable values: 90 degree total range (emulation); -45 = left; +45 = right; 0 = centre
+// this direction (-'ve angles are LEFT turns) has been verified on the roller skate boat, with the wheel at the back emulating the rudder properly
+// except the servo is upside down; so on the real boat, -'ve angles are RIGHT turns
+
+
+
+
+void setrudder(float ang) {
+//fill this in with the code to interface with pololu
+
+    //int servo_num = 1;
+    int pos; //position
+    
+    
+    
+
+//check input, and change is appropriate
+    constrain(ang,-30,30);//ang is returned as value from -30 to 30
+    pos=90+ang;//90 is the midpoint of the servo's rotation and is added or subtracted to turn 30 degree right or left.
+    rudderServo.write(pos);
+    //pos = (ang + 45) * rudderDir * 254.0 / 90.0;//convert from 180 degree range, -90 to +90 angle to a 0 to 256 maximum position range
+    //servo_command(servo_num,pos,0);
+    //rudderVal = ang;
+}
+
+void setSails(float ang)
+//this could make more sense conceptually for sails if it mapped 0 to 90 rather than -45 to +45
+{
+    setJib(ang);
+    setMain(ang);
+}
+
+void setJib(float ang)
+//currently using setsails to call both main and jib servos
+{
+    //int servo_num = 3;
+    int pos;
+
+    constrain(ang, 1,100);//returns ang in a percent to turn the motor out of 100%
+    pos = (ang/100.0) * 180;//set percent to angle
+    jibServo.write(pos);//move motor to angle
+    //pos = ang*110.0/100.0; //tweaking for gaelforce 2,
+    //servo_command(servo_num,pos,0);
+    //jibVal = ang;
+}
+
+void setMain(float ang)
+//code for setting main sail only
+{
+    //int servo_num = 2;
+    int pos;
+
+    constrain(ang,1,100);//returns ang in a percent to turn the motor out of 100%
+    pos = (ang/100.0) * 180;//set percent to angle
+    mainServo.write(pos);//move motor to angle
+    //pos = (ang + 50)*104.0/100.0;  //tweaking for gaelforce 2, check if smartwinch fuse blows
+    //servo_command(servo_num,pos,0);
+    //mainVal = ang;
+}
+
+
---
+++ Testing_Code/hall_effect_sensor/hall_effect_sensor.ino
@@ -0,0 +1,46 @@
+/*
+Testing the Hall Effect sensor
+Valerie and Lazlo
+July 2012
+
+http://www.gmw.com/magnetic_sensors/sentron/2sa/GMW360ASM.html
+http://www.gmw.com/magnetic_sensors/ametes/documents/Ametes_360ASM_Spec_07_May_2007.pdf
+
+The short version:
+Analog out gives 2.5V at 0 degrees, 0.5V at -180 degrees, 4.5V at 180 degrees
+Sensor can reset its zero angle by temporarily connecting the analog out pin to +5V. This is then stored in non-volatile memory
+
+Range of analag input is 0-1023
+*/
+
+#define ANGLE_PIN A5
+#define ZERO_VOLTS 512
+#define NO_FIELD_PIN 2
+int input_angle;
+int input_no_field;
+int angle;
+
+void setup() {
+   Serial.begin(9600); 
+   pinMode(ANGLE_PIN, INPUT);  
+   pinMode(NO_FIELD_PIN, INPUT);
+   input_angle = 0;
+}
+
+
+void loop() {
+  
+  input_angle = analogRead(ANGLE_PIN);
+  input_no_field = digitalRead(NO_FIELD_PIN);
+  angle = (input_angle - ZERO_VOLTS)*(180.0/408.0); //scale factor for mapping degrees to analog input values
+  if(input_no_field == LOW)
+  {
+    Serial.println("No magnet...");
+  }
+  else
+  {
+    Serial.println(angle);
+  }
+  delay(500);
+  
+}  
